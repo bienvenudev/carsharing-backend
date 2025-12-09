eae441edb337b98e2f741811ea7033616c6ca445
bienvenudev <cbienvenu007@gmail.com>
2025-12-09 23:10:47 +0200

    Update CreateCarDto to make info field optional

--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -1,4 +1,4 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateCarDto {
   @IsNumber()
@@ -17,5 +17,6 @@ export class CreateCarDto {
   licensePlate: string;
 
   @IsString()
-  info: string;
+  @IsOptional()
+  info?: string;
 }
